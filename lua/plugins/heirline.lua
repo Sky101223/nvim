44613fa79c17b059c5d29988dc8ad9dfc22cadca
Sky101223 <lixxlilol@163.com>
2025-12-06 21:55:44 +0800

    set: Shortcut keys for tabline

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -1,17 +1,22 @@
-vim.pack.add({
-	{ src = "https://github.com/linrongbin16/lsp-progress.nvim" },
-	{ src = "https://github.com/rebelot/heirline.nvim" },
-})
+vim.pack.add {
+  { src = 'https://github.com/linrongbin16/lsp-progress.nvim' },
+  { src = 'https://github.com/rebelot/heirline.nvim' },
+}
 
 vim.opt.cmdheight = 0
 
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
-	group = vim.api.nvim_create_augroup("SetupHeirline", { clear = true }),
-	once = true,
-	callback = function()
-		require("lsp-progress").setup()
-		require("heirline").setup({
-			statusline = require("config.heirline.statusline"),
-		})
-	end,
+vim.keymap.set('n', '<leader>tt', function()
+  vim.o.showtabline = vim.o.showtabline == 0 and 2 or 0
+end, { desc = 'Toggle tabline' })
+
+vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
+  group = vim.api.nvim_create_augroup('SetupHeirline', { clear = true }),
+  once = true,
+  callback = function()
+    require('lsp-progress').setup()
+    require('heirline').setup {
+      statusline = require 'config.heirline.statusline',
+      -- tabline = require 'config.heirline.everforest.tabline',
+    }
+  end,
 })
