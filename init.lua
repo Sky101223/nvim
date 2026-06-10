d0673ae082195d46f74ac746f07ec508b123f16b
Sky101223 <lixxlilol@163.com>
2026-06-10 13:25:56 +0800

    Skynvim

--- init.lua
+++ init.lua
@@ -1,42 +1,18 @@
--- Plugins
-vim.pack.add(require 'manager', { load = false })
-
 -- Basic Configuration
+require 'config.autocmds'
 require 'config.options'
 require 'config.keymaps'
-require 'config.autocmds'
 
--- Themes & Statusline
-require 'plugins.everforest'
-require 'plugins.kanagawa'
-require 'plugins.vague'
--- require 'plugins.nord'
-require 'plugins.catppuccin'
-require 'plugins.nightfox'
--- require 'plugins.heirline'
-require 'plugins.lualine'
-require 'plugins.tokyonight'
+-- Plugins
+local plugins = require 'plugins'
+vim.pack.add(plugins.sites, { load = false })
+for _, file in ipairs(plugins.config_files) do
+  local mod = 'plugins.' .. file
+  require(mod)
+end
 
--- Language
--- require 'plugins.completion' -- Blink
--- require 'plugins.autopairs' -- Auto Pair
-require 'plugins.conform' -- Conform
-require 'plugins.overseer' -- Overseer
-require 'plugins.debugging' -- Debug
-require 'config.lsp' -- LSP
-
--- Editor Layout & Search
-require 'plugins.oil' -- File explorer
--- require 'plugins.pdf' -- PDF
-require 'plugins.snacks' -- Tools
-require 'plugins.mini' -- Tools
--- require 'plugins.which-key' -- Keybinding hints
-
--- Editing Utilities
--- require 'plugins.comments' -- Comments
-require 'plugins.leetcode' -- LeetCode
-
-require 'plugins.gitsigns' -- Git
+-- LSP
+require 'config.lsp'
 
 require('vim._core.ui2').enable {
   enable = false,
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -34,19 +34,19 @@ require('mini.surround').setup {
   },
 }
 
--- require('mini.pick').setup()
+require('mini.pick').setup()
 -- require('mini.git').setup()
 require('mini.tabline').setup()
--- require('mini.indentscope').setup()
--- require('mini.notify').setup {
---   window = {
---     winblend = 0,
---     border = 'rounded',
---     zindex = 50,
---   },
--- }
+require('mini.indentscope').setup()
+require('mini.notify').setup {
+  window = {
+    winblend = 0,
+    border = 'rounded',
+    zindex = 50,
+  },
+}
 require('mini.pairs').setup()
--- require('mini.comment').setup()
+require('mini.comment').setup()
 
 vim.o.showtabline = 0
 local function toggle_tabline()
@@ -64,17 +64,17 @@ local map = function(key, func, desc)
 end
 
 map('<leader>tt', toggle_tabline, 'Toggle tabline')
--- map('<leader>ff', ':Pick files<CR>', 'Smart find file')
--- map('<leader>fh', ':Pick help<CR>', 'Find in help')
--- map('<leader>fw', ':Pick grep<CR>', 'Find content')
--- map('<leader>fW', ':Pick grep_live<CR>', 'Find content(live)')
--- map('<leader>fc', ':e $MYVIMRC<CR>', 'Setting')
--- map('<leader>gb', function()
---   require('mini.git').show_at_cursor()
--- end, 'Git blame line (mini.git)')
--- map('<leader>gg', function()
---   vim.cmd 'terminal lazygit'
---   vim.wo.winfixheight = true
---   vim.bo.filetype = 'lazygit'
--- end, 'Open LazyGit in terminal')
--- map('<leader><leader>', ':Pick buffers<CR>', 'Find buffers')
+map('<leader>ff', ':Pick files<CR>', 'Smart find file')
+map('<leader>fh', ':Pick help<CR>', 'Find in help')
+map('<leader>fw', ':Pick grep<CR>', 'Find content')
+map('<leader>fW', ':Pick grep_live<CR>', 'Find content(live)')
+map('<leader>ec', ':e $MYVIMRC<CR>', 'Setting')
+map('<leader>gb', function()
+  require('mini.git').show_at_cursor()
+end, 'Git blame line (mini.git)')
+map('<leader>gg', function()
+  vim.cmd 'terminal lazygit'
+  vim.wo.winfixheight = true
+  vim.bo.filetype = 'lazygit'
+end, 'Open LazyGit in terminal')
+map('<leader><leader>', ':Pick buffers<CR>', 'Find buffers')
