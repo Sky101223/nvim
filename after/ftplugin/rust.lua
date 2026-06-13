3dd75dfd270459a1e1cd138da531fc764f2407cc
Sky101223 <lixxlilol@163.com>
2026-06-13 21:43:26 +0800

    fix: rust language

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -4,3 +4,20 @@ end
 vim.b.did_my_ftplugin = true
 
 vim.lsp.enable 'rust-analyzer'
+vim.keymap.set('n', '<localleader>r', function()
+  vim.cmd 'w'
+  vim.cmd 'botright 10split | terminal cargo run'
+  vim.cmd 'startinsert'
+end, { desc = 'Rust: cargo run' })
+
+vim.keymap.set('n', '<localleader>b', function()
+  vim.cmd 'w'
+  vim.cmd 'botright rightbelow 10split | terminal cargo build'
+  vim.cmd 'startinsert'
+end, { desc = 'Rust: cargo build' })
+
+vim.keymap.set('n', '<localleader>t', function()
+  vim.cmd 'w'
+  vim.cmd 'botright rightbelow 10split | terminal cargo test'
+  vim.cmd 'startinsert'
+end, { desc = 'Rust: cargo test' })
--- lsp/rust-analyzer.lua
+++ lsp/rust-analyzer.lua
@@ -1,10 +1,11 @@
 return {
+  cmd = { 'rust-analyzer' },
+  filetypes = { 'rust' },
+  root_markers = { 'Cargo.toml', '.git' },
   settings = {
     ['rust-analyzer'] = {
       checkOnSave = true,
-      procMacro = {
-        enable = true,
-      },
+      procMacro = { enable = true },
     },
   },
 }
