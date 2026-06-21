193358ce31617e4b63ade9c1c944e69771f03cc5
Sky101223 <lixxlilol@163.com>
2026-06-21 18:04:19 +0800

    Skynvim

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -18,10 +18,14 @@ vim.diagnostic.config {
   severity_sort = true,
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = '',
-      [vim.diagnostic.severity.WARN] = '',
-      [vim.diagnostic.severity.INFO] = '',
-      [vim.diagnostic.severity.HINT] = '',
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '',
+      -- [vim.diagnostic.severity.ERROR] = '',
+      -- [vim.diagnostic.severity.WARN] = '',
+      -- [vim.diagnostic.severity.INFO] = '',
+      -- [vim.diagnostic.severity.HINT] = '',
     },
     numhl = {
       [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
