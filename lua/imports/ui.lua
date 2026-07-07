eea21d31cceff43116380718ecb0d46a25a202ac
Sky101223 <lixxlilol@163.com>
2026-07-08 07:58:04 +0800

    Skynvim

--- lua/imports/ui.lua
+++ lua/imports/ui.lua
@@ -76,40 +76,40 @@ vim.cmd 'colorscheme kanagawa'
 vim.cmd.hi 'statusline guibg=NONE'
 
 -- Lualine
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = { 'mode' },
-    lualine_b = {
-      {
-        'branch',
-        icon = '󰘬',
-      },
-      'diff',
-    },
-    lualine_c = {
-      {
-        'filename',
-        path = 1,
-      },
-      'diagnostics',
-    },
-    lualine_x = {
-      'encoding',
-      'fileformat',
-      {
-        'filetype',
-        color = {
-          gui = 'bold',
-        },
-      },
-    },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' },
-  },
-}
+-- require('lualine').setup {
+--   options = {
+--     icons_enabled = true,
+--     theme = 'auto',
+--     component_separators = { left = '', right = '' },
+--     section_separators = { left = '', right = '' },
+--   },
+--   sections = {
+--     lualine_a = { 'mode' },
+--     lualine_b = {
+--       {
+--         'branch',
+--         icon = '󰘬',
+--       },
+--       'diff',
+--     },
+--     lualine_c = {
+--       {
+--         'filename',
+--         path = 1,
+--       },
+--       'diagnostics',
+--     },
+--     lualine_x = {
+--       'encoding',
+--       'fileformat',
+--       {
+--         'filetype',
+--         color = {
+--           gui = 'bold',
+--         },
+--       },
+--     },
+--     lualine_y = { 'progress' },
+--     lualine_z = { 'location' },
+--   },
+-- }
